a3a1d94d4333a2ad9fde74f398c0d8e47ccd8652
leandrosax10 <leandrosantosti730@gmail.com>
2021-08-25 15:01:54 -0300

    Atualização

--- app/src/main/java/br/com/leandro/listacontatos/ContactAdapter.kt
+++ app/src/main/java/br/com/leandro/listacontatos/ContactAdapter.kt
@@ -1,5 +1,6 @@
 package br.com.leandro.listacontatos
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -10,17 +11,21 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
-    class  ContactAdapterViewHolder (itemView: View): RecyclerView.ViewHolder(itemView){
 
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    class  ContactAdapterViewHolder (itemView: View): RecyclerView.ViewHolder(itemView){
+            fun bind(contact: Contact){
+
+         }
     }
 }
